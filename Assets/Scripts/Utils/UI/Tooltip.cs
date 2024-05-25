d8ae8c52479d8e59d7fb28ac3d5574aae0fef111
Jaggard <83131497+TheGrandJaggard@users.noreply.github.com>
2024-05-25 10:51:55 -0700

    Add bounce to items using tweening

--- Assets/Scripts/Utils/UI/Tooltip.cs
+++ Assets/Scripts/Utils/UI/Tooltip.cs
@@ -41,43 +41,14 @@ namespace SDVA.Utils.UI
             var mPos = Input.mousePosition;
 
             var left = mPos.x + tooltipSize.x > Screen.width;
-            Debug.Log($"var {left} = {mPos.x} + {tooltipSize.x} > {Screen.width};");
             var above = mPos.y - tooltipSize.y < 0;
-            Debug.Log($"var {above} = {mPos.y} - {tooltipSize.y} < 0");
 
             GetComponent<RectTransform>().pivot = GetPivots(above, left);
             transform.position = mPos;
         }
         
-
-        // private void PositionTooltip(RectTransform anchorRect = null)
-        // {
-        //     // Required to ensure corners are updated by positioning elements.
-        //     Canvas.ForceUpdateCanvases();
-
-        //     var anchorCorners = new Vector3[4];
-        //     if (anchorRect) { anchorRect.GetWorldCorners(anchorCorners); }
-        //     var corners = new Vector3[4];
-        //     GetComponent<RectTransform>().GetWorldCorners(corners);
-        //     Debug.Log($"{corners[0]}, {corners[1]}, {corners[2]}, {corners[3]}");
-        //     Debug.Log($"{anchorCorners[0]}, {anchorCorners[1]}, {anchorCorners[2]}, {anchorCorners[3]}");
-
-        //     bool below = (anchorRect ? anchorCorners[0].y + anchorCorners[3].y / 2
-        //                   : Input.mousePosition.x) > Screen.height / 2;
-        //     bool right = (anchorRect ? anchorCorners[0].x + anchorCorners[1].x / 2
-        //                   : Input.mousePosition.y) < Screen.width / 2;
-
-        //     int slotCorner = GetCornerIndex(below, right);
-        //     int tooltipCorner = GetCornerIndex(below, !right);
-
-        //     transform.position = anchorCorners[slotCorner] - corners[tooltipCorner] + transform.position;
-        // }
-
-        // pivots 0,1  1,1
-        //        0,0  1,0
         private Vector2 GetPivots(bool above, bool left)
         {
-            Debug.Log($"Above: {above}, left: {left}");
             if (above && !left) { return new Vector2(0, 0); }       // Bottom Left
             else if (!above && !left) { return new Vector2(0, 1); } // Top Left
             else if (!above && left) { return new Vector2(1, 1); }  // Top Right
