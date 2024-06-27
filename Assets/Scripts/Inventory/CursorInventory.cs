4cb88b753385aa4941bde1728fe56cf414237845
Jaggard <83131497+TheGrandJaggard@users.noreply.github.com>
2024-06-27 11:34:56 -0700

    Move CursorInventory out of UI folder and clean up

--- Assets/Scripts/Inventory/CursorInventory.cs
+++ Assets/Scripts/Inventory/CursorInventory.cs
@@ -1,18 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
 using SDVA.Utils.UI.ItemMovement;
 using SDVA.InventorySystem;
+using UnityEngine;
 using UnityEngine.EventSystems;
-using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
 namespace SDVA.UI.InventorySystem
 {
-    public class CursorInventory : MonoBehaviour, IItemTransitionContainer<BaseItem>
+    public class CursorInventory : MonoBehaviour, IItemContainer<BaseItem>
     {
         // CONFIG DATA
-        [SerializeField] CursorItemIcon icon;
-        [SerializeField] bool useClickNDrop;
+        [SerializeField] InventoryItemIcon icon;
+
+        [Header("Inventory item movement settings")]
+        [Tooltip("Whether items can be picked up by clicking")]
+        [SerializeField] bool useClick;
+        [Tooltip("Whether items are dropped after a drag ends\n(Not recommended, but makes for a simpler system when other settings are turned off)")]
         [SerializeField] bool useDragging;
+        [Tooltip("Whether right click should pickup and drop single items (WIP)")]
+        [SerializeField] bool enableRightClick;
+        [Tooltip("Whether shift left clicking should pickup and drop half available items (WIP)")]
+        [SerializeField] bool enableShiftClick;
+        [Tooltip("Whether to pickup all items of type on double left click (WIP)")]
+        [SerializeField] bool enableDoubleClick;
+        [Tooltip("Whether shift double left clicking should spread all items of type to all available slots (WIP)")]
+        [SerializeField] bool enableShiftDoubleClick;
+        [Tooltip("Whether similar items should be picked up during drag (WIP)")]
+        [SerializeField] bool aggregateOnDragPickup;
+        [Tooltip("Whether items should be spread out between slots dragged over during drop (WIP)")]
+        [SerializeField] bool spreadOnDragDrop;
 
         private IItemSource<BaseItem> mostRecentSource; // Used for dragging returns
 
@@ -23,7 +39,7 @@ namespace SDVA.UI.InventorySystem
 
         // PUBLIC
 
-        public int MaxAcceptable(BaseItem item) => item?.GetMaxStackSize() ?? 0;
+        public int MaxAcceptable(BaseItem item) => item != null ? item.GetMaxStackSize() : 0;
 
         public int AddItems(BaseItem item, int number) => inventory.AddToSlot(slot, item, number);
 
@@ -67,7 +83,7 @@ namespace SDVA.UI.InventorySystem
                 var hitObject = hitResult.gameObject;
 
                 if (hitObject.TryGetComponent<IItemSource<BaseItem>>(out var source) &&
-                    (useClickNDrop || useDragging))
+                    (useClick || useDragging))
                 {
                     var itemsMoved = MoveItem<BaseItem>.MoveBetween(source, this);
                     mostRecentSource = source;
@@ -75,7 +91,7 @@ namespace SDVA.UI.InventorySystem
                 }
 
                 if (hitObject.TryGetComponent<IItemDestination<BaseItem>>(out var destination) &&
-                    useClickNDrop)
+                    useClick)
                 {
                     var itemsMoved = MoveItem<BaseItem>.MoveBetween(this, destination);
                     if (itemsMoved > 0) { break; }
@@ -115,7 +131,7 @@ namespace SDVA.UI.InventorySystem
 
         private void CancelDrag(InputAction.CallbackContext context)
         {
-            if (useClickNDrop || !useDragging) { return; }
+            if (useClick || !useDragging) { return; }
             if (mostRecentSource is IItemContainer<BaseItem> mostRecentSourceContainer)
             {
                 MoveItem<BaseItem>.MoveTo(this, mostRecentSourceContainer);
